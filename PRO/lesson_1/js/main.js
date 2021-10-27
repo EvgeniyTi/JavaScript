fae5ee5cd3f408372208221484db9f2a82cedf5a
evgeniy_timoshenko <kostetanet@mail.ru>
2021-10-27 21:47:59 +0300

    update

---
+++ PRO/lesson_1/js/main.js
@@ -0,0 +1,25 @@
+const products = [
+    { id: 1, title: 'Notebook', price: 2000 },
+    { id: 2, title: 'Mouse', price: 20 },
+    { id: 3, title: 'Keyboard', price: 200 },
+    { id: 4, title: 'Gamepad', price: 50 },
+];
+//Функция для формирования верстки каждого товара
+//Добавить в выводе изображение
+const renderProduct = (title, price, img = "1.png") => {
+    return `<div class="product-item">
+                <img src="${img}">
+                <h3>${title}</h3>
+                <p>${price}</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+};
+const renderPage = list => {
+    const productsList = list.map(item => renderProduct(item.title, item.price)).join(' ');
+
+    console.log(productsList);
+    document.querySelector('.products').innerHTML = productsList;
+
+};
+
+renderPage(products);
